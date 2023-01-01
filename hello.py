0d35c320a0764307803026666ad7df858b8a3d08
Pichaya Tandayya <tpichaya@gmail.com>
2023-01-01 08:39:27 +0000

    summation initial structure

---
+++ hello.py
@@ -0,0 +1,14 @@
+def adding(x, y):
+    return x, y
+
+
+def summation(x, y):
+    return x + y
+
+
+# x=1
+# y=2
+result = summation(1, 2)
+# print(f"This is the add: {x}, {y}, {adding(x,y)}")
+# print(f"This is the sum: {x}, {y}, {summation(x,y)}")
+print(f"This is the sum: {1}, {2}, {result}")
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import summation
+
+
+def test_summation():
+    assert summation(1, 2) == 3
+    # assert summation(2,2) == 3
